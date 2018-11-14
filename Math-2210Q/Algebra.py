a579d08c4c07b59b3d8b8b460095fb5c8dac8d49
kmp16101 <klauss.preising@uconn.edu>
2018-11-14 12:37:43 -0500

    Algebra

--- Math-2210Q/Algebra.py
+++ Math-2210Q/Algebra.py
@@ -1,10 +1,15 @@
+from Matrix import *
+
+# gets the inverse of a matrix
 def inverse_matrix(mat):
     pass
 
+# reduced echelon form
+# Eventually PA = LU
 def reduced_echelon_form(mat):
     pass
 
+
+# Eigenvalues of a matrix
 def eigenvalues(mat):
     pass
-
-
